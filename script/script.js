99181ee6ac5e0c7dc724e346698a69f49cf6f340
RenaudJean <95225657+Renaudjean@users.noreply.github.com>
2022-05-06 16:52:23 +0200

    Finsihed(added the regex and this actually works)

--- script/script.js
+++ script/script.js
@@ -2,28 +2,51 @@ const form = document.querySelector('.contact-form');
 const button = document.querySelector('#form-button');
 let emailIn = document.querySelector('#Email');
 let titleIn = document.querySelector('#Title');
-let messageIn = document.querySelector('#form-button');
+let messageIn = document.querySelector('#Message');
+let ResponseMessage = document.querySelector("#ResponseMessage");
+let overlay = document.querySelector(".overlay");
+let overlayBtn = document.querySelector("#overlay-btn");
+const emailReg = /(^[a-zA-Z0-9_.]+[@]{1}[a-z0-9]+[\.][a-z]+$)/mg;
 
 
 button.addEventListener("click", function(){
 
-let xmlhttp = new XMLHttpRequest();
-let information = `email=${emailIn.value}&titre=${titleIn.value}&message=${messageIn.value}`;
+    form.onsubmit = e => {
+            e.preventDefault();
+        if(emailReg.test(emailIn.value) ){   
+            let xmlhttp = new XMLHttpRequest();
+            
+            let information = `email=${emailIn.value}&title=${titleIn.value}&message=${messageIn.value}`;
+        
+            xmlhttp.onload = function (){
+                const serverResponse = this.responseText;
+            }
+            xmlhttp.open("POST","./script/sendEmail.php", true);
 
-xmlhttp.onload = function (){
-    const serverResponse = this.responseText;
-}
-xmlhttp.open("POST","./sendEmail.php", true);
+            xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-xmlhttp.setRequestHeader("Content-Type", "application/json");
+            xmlhttp.send(information);
+        
 
-xmlhttp.send(information);
-xmlhttp.onreadystatechange = function (){
-        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-        ResponseMessage.innerHTML = xhttp.responseText;
-        emailIn.value="";
-        titleIn.value="";
-        messageIn.value="";
+            xmlhttp.onreadystatechange = function (){
+                
+                    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                        
+                        ResponseMessage.innerHTML = xmlhttp.responseText;
+                        overlay.classList.remove("hidden");
+                        overlayBtn.addEventListener("click",function(){
+                        overlay.classList.add("hidden");
+                    })
+                        emailIn.value="";
+                        titleIn.value="";
+                        messageIn.value="";
+                
+            }
+         }
+        }
+        else{
+            alert("email invalid!")
         }
     }
 })
+
--- script/sendEmail.php
+++ script/sendEmail.php
@@ -1,9 +1,34 @@
 <?php
-// if (isset($_POST['Email'])) {
-//    $email_to = "renaud06goubelle@gmail.com";
-//    $email_subject = $_POST['subject'];
-//    echo $email_subject;
+   ini_set('SMTP','smtp.google.com');
+   ini_set("smpt_port", 587);
+   $email_from =  $_POST['email'];
+   $email_subject =  $_POST['title'];
+   $email_message = $_POST['message'];
 
-// }
+   // Configure JSON response
+   if(!filter_var($email_from, FILTER_VALIDATE_EMAIL)){  //check if the mail format is valid or not
+      echo 'Email Format not valid';
+   }else{
+      $subject = $email_subject; 
+      $email_to = "renaud06goubelle@gmail.com";
 
-echo "lol";
+      //Add email of user
+      $headers = 'From:'.$email_from . "\r\n";   
+
+      $message = "From: ".$email_from." \n".$email_message;
+      // $headers .= "Content-type: text/html\r\n";
+
+      $emailSend = mail ($email_to, $subject, $message, $headers);  //send the email with all the parameters we need
+         
+      if( $emailSend == true ) {          //if the email send ot not we send a message 
+         echo "Message sent successfully...";
+      }else {
+         echo "Message could not be sent..........";
+      }
+       
+   }
+  
+
+  
+ 
+?>
